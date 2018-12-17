d8ece0add62057eade5b5828178feee4fd10b2bf
Sayie <westenme@163.com>
2018-12-17 17:35:46 +0800

    Initial commit

---
+++ src/main/java/com/sayie/weibinbin/WeibinbinApplication.java
@@ -0,0 +1,14 @@
+package com.sayie.weibinbin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeibinbinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeibinbinApplication.class, args);
+    }
+
+}
+
