f4aaf445a11d23cc1d7dcff842cec859983d0d3c
Sayie <westenme@163.com>
2019-01-08 17:43:26 +0800

    second push

---
+++ src/main/java/com/sayie/weibinbin/dao/UserDao.java
@@ -0,0 +1,11 @@
+package com.sayie.weibinbin.dao;
+
+import com.sayie.weibinbin.entity.User;
+
+/**
+ * @author Sayie
+ * @date 2019/1/8 17:35
+ */
+public interface UserDao {
+    User findUser();
+}
